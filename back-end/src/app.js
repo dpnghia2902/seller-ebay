4da569c33232fd200c692814de6bef2e0ef1a2e8
HoangNV <hoangptkdhy@gmail.com>
2025-10-06 18:49:19 +0700

    add swagger

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');  // Cấu hình kết nối MongoDB
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output.json');
 
 dotenv.config();
 connectDB();  // Kết nối MongoDB
@@ -10,6 +12,14 @@ const app = express();
 // Middleware để phân tích body của yêu cầu
 app.use(express.json());
 
+// Swagger UI - chỉ enable trong development
+if (process.env.NODE_ENV !== 'production') {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile, {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: "E-commerce API Docs"
+  }));
+}
+
 // Import các routes
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
@@ -18,6 +28,15 @@ const couponRoutes = require('./routes/couponRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const authRoutes = require('./routes/authRoutes');
+
+// Root endpoint
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to API',
+    documentation: '/api-docs'
+  });
+});
+
 // Sử dụng các routes
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
